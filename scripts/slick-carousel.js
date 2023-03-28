72bb0f5520b997cc12768c2738b5fda8fbdbddf0
Joaquín Solla Vázquez <joaquin.solla@udc.es>
2023-03-28 03:14:44 +0200

    Updated galeria.html UI and elements (v1).

---
+++ scripts/slick-carousel.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slick-galeria-intro').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+    });
+});
