8322d1899bb97f3d2f19f1919b5d783b72bd6758
Joaquín Solla Vázquez <joaquin.solla@udc.es>
2024-02-26 20:42:26 +0100

    Updated gallery

--- scripts/gallery_management.js
+++ scripts/gallery_management.js
@@ -5,24 +5,29 @@ function toggleSectionDisplay(index) {
     const capelaButton = document.getElementById("capela-button");
     const monteButton = document.getElementById("monte-button");
     const muinoButton = document.getElementById("muino-button");
+    const xornada2024Button = document.getElementById("xornada-2024-button");
 
     if (hipicaButton.textContent.charAt(0) === "\u2022") hipicaButton.textContent = hipicaButton.textContent.slice(2);
     if (aldeaButton.textContent.charAt(0) === "\u2022") aldeaButton.textContent = aldeaButton.textContent.slice(2);
     if (capelaButton.textContent.charAt(0) === "\u2022") capelaButton.textContent = capelaButton.textContent.slice(2);
     if (monteButton.textContent.charAt(0) === "\u2022") monteButton.textContent = monteButton.textContent.slice(2);
     if (muinoButton.textContent.charAt(0) === "\u2022") muinoButton.textContent = muinoButton.textContent.slice(2);
+    if (xornada2024Button.textContent.charAt(0) === "\u2022") xornada2024Button.textContent = xornada2024Button.textContent.slice(2);
+
 
     const hipica = document.getElementById("hipica");
     const aldea = document.getElementById("aldea");
     const capela = document.getElementById("capela");
     const monte = document.getElementById("monte");
     const muino = document.getElementById("muino");
+    const xornada2024 = document.getElementById("xornada-2024");
 
     hipica.style.display = "none";
     aldea.style.display = "none";
     capela.style.display = "none";
     monte.style.display = "none";
     muino.style.display = "none";
+    xornada2024.style.display = "none";
 
     switch (index){
         case 0:
@@ -44,6 +49,10 @@ function toggleSectionDisplay(index) {
         case 4:
             muino.style.display = "block";
             muinoButton.textContent = "\u2022\u00a0" + muinoButton.textContent;
+            break;
+        case 5:
+            xornada2024.style.display = "block";
+            xornada2024Button.textContent = "\u2022\u00a0" + xornada2024Button.textContent;
             break;
         default:
             hipica.style.display = "block";
